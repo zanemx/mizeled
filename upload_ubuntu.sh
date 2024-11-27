ea9da4e95de990491206902093eefe8b61cf8ed5
zanemx <zanemx@gmail.com>
2024-11-27 00:54:31 -0500

    updating stack

--- upload_ubuntu.sh
+++ upload_ubuntu.sh
@@ -1,7 +1,3 @@
 echo "update nginx ubuntu fastapi server"
-ssh -i ~/.ssh/aws-ubuntu-24.pem ubuntu@3.89.88.232
-cd apis/mizeled
-git pull origin main
-exit
-
+ssh -i ~/.ssh/aws-ubuntu-24.pem ubuntu@3.89.88.232 -t 'source ~/.zshrc; cd apis/mizeled; git pull origin main; exit;'
 echo "finished updating server"
